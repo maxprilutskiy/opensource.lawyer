e094377953023601a005ec46ad1c9c9846b64a0b
maxprilutskiy <maks.prilutskiy@gmail.com>
2024-04-07 11:53:11 -0700

    feat: add remix project

---
+++ app/root.tsx
@@ -0,0 +1,41 @@
+import { LinksFunction } from "@remix-run/cloudflare";
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import tailwindCss from '~/tailwind.css?url';
+import { NextUIProvider } from "@nextui-org/react";
+
+export const links: LinksFunction = () => [
+  { rel: 'preload', as: 'style', href: tailwindCss },
+  { rel: 'stylesheet', href: tailwindCss },
+];
+
+export function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
+export default function App() {
+  return (
+    <NextUIProvider>
+      <Outlet />
+    </NextUIProvider>
+  );
+}
