b011638bf80eb6731eafe6aec6e609f83dd6de3f
maxprilutskiy <maks.prilutskiy@gmail.com>
2024-04-07 14:03:41 -0700

    feat: upd loader

--- app/api/chat/engine/loader.ts
+++ app/api/chat/engine/loader.ts
@@ -1,9 +1,23 @@
-import { SimpleDirectoryReader } from "llamaindex";
+import { Document } from "llamaindex";
 
 export const DATA_DIR = "./data";
 
+const remoteLocation = 'https://github.com/maxprilutskiy/oss-license/blob/main/data';
+const licenseIds = [
+  'mit',
+  'agpl',
+];
+
 export async function getDocuments() {
-  return await new SimpleDirectoryReader().loadData({
-    directoryPath: DATA_DIR,
-  });
+  return await Promise.all(
+    licenseIds.map((id) => 
+      fetch(`${remoteLocation}/${id}.md?raw=true`)
+        .then((response) => response.text())
+        .catch((error) => {
+          console.error(`Failed to fetch license ${id}: ${error}`);
+          return '';
+        })
+        .then((text) => new Document({ id_: id, text })),
+    ),
+  );
 }
